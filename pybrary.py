731051aedd9e6d5af48be144fce63f018eeaaec9
Mona Lisa <147712158+thusiru@users.noreply.github.com>
2024-09-05 12:47:24 +0530

    Move welcome note to inputs.py

--- pybrary.py
+++ pybrary.py
@@ -2,15 +2,7 @@ from books import search_books
 from inputs import parse
 
 
-RED = "\033[91m"
-RESET = "\033[0m"
-
-
 def main():
-    print(
-        f"Welcome to Pybrary! For help type {RED}'--help'{RESET} and press enter. For exit type {RED}'--exit'{RESET} and hit enter."
-    )
-
     while True:
         text = parse(input("Input: "))
         if text is None:
