16f28f6ca55f55be5df6eb3cd9f99ed28b000233
Mona Lisa <147712158+thusiru@users.noreply.github.com>
2024-09-05 12:48:57 +0530

    Add welcome note to inputs.py
    Correct the exit code in parse function in inputs.py from exit to exit(0)

--- inputs.py
+++ inputs.py
@@ -4,13 +4,20 @@ from sys import exit
 RED = "\033[91m"
 RESET = "\033[0m"
 
+first_time = True
+if first_time:
+    print(
+        f"Welcome to Pybrary! For help type {RED}'--help'{RESET} and press enter. For exit type {RED}'--exit'{RESET} and hit enter."
+    )
+    first_time = False
+
 
 def parse(text):
     match text.strip().lower():
         case "--exit":
             print("Exiting the program...")
             print("Have a nice day! 🙂")
-            exit
+            exit(0)
         case "--help":
             print(f"     {RED}--exit{RESET} - Exit the Program")
             print(f"     {RED}--help{RESET} - Show help")
